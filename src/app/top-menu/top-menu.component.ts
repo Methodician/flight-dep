ce32b9e6a9212a748542628c820486bf276d2ae2
Methodician <jacobgjohnston@gmail.com>
2016-12-05 11:39:28 -0800

    Animated top menu for mobile

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -1,9 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
   selector: 'top-menu',
   templateUrl: './top-menu.component.html',
-  styleUrls: ['./top-menu.component.css']
+  styleUrls: ['./top-menu.component.css'],
+  animations: [
+    trigger('navCollapsed', [
+      state('true', style({ height: '0px' })),
+      state('false', style({ height: '*' })),
+      transition('1 => 0', [animate('300ms ease-in')]),
+      transition('0 => 1', [animate('300ms ease-out')])
+    ]),
+    trigger('topBarCollapsed', [
+      state('true', style({ transform: 'rotate(0) translateY(0)' })),
+      state('false', style({ transform: 'rotate(45deg) translateY(200%) translateX(20%)' })),
+      transition('1 <=> 0', [animate('300ms')])
+    ]),
+    trigger('middleBarCollapsed', [
+      state('true', style({ opacity: '*' })),
+      state('false', style({ opacity: '0' })),
+      transition('1 <=> 0', [animate('300ms')])
+    ]),
+    trigger('bottomBarCollapsed', [
+      state('true', style({ transform: 'rotate(0) translateY(0)' })),
+      state('false', style({ transform: 'rotate(-45deg) translateY(-200%) translateX(20%)' })),
+      transition('1 <=> 0', [animate('300ms')])
+    ])
+  ]
 })
 export class TopMenuComponent implements OnInit {
 
