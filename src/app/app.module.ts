6642e01281e18a16fd9b29da9ea0574a30603f26
Methodician <jacobgjohnston@gmail.com>
2016-12-05 18:06:35 -0800

    fixed up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { CollapseDirective } from 'ng2-bootstrap';
 
 const Routes: Routes = [
-  { path: 'work', component: HomeComponent },
+  { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'contact', component: ContactComponent },
   { path: '', component: HomeComponent }
