6aca2deb6cbfb3e495c4104d00039f57f87b0e22
Tricia Mendoza <tvmendoza99@gmail.com>
2016-12-05 17:54:33 -0800

    edited nav color theme and functions

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -8,23 +8,23 @@ import { Component, OnInit, trigger, state, style, transition, animate } from '@
     trigger('navCollapsed', [
       state('true', style({ height: '0px' })),
       state('false', style({ height: '*' })),
-      transition('1 => 0', [animate('300ms ease-in')]),
-      transition('0 => 1', [animate('300ms ease-out')])
+      transition('1 => 0', [animate('170ms ease-in')]),
+      transition('0 => 1', [animate('200ms ease-out')])
     ]),
     trigger('topBarCollapsed', [
       state('true', style({ transform: 'rotate(0) translateY(0)' })),
       state('false', style({ transform: 'rotate(45deg) translateY(200%) translateX(20%)' })),
-      transition('1 <=> 0', [animate('300ms')])
+      transition('1 <=> 0', [animate('170ms')])
     ]),
     trigger('middleBarCollapsed', [
       state('true', style({ opacity: '*' })),
       state('false', style({ opacity: '0' })),
-      transition('1 <=> 0', [animate('300ms')])
+      transition('1 <=> 0', [animate('170ms')])
     ]),
     trigger('bottomBarCollapsed', [
       state('true', style({ transform: 'rotate(0) translateY(0)' })),
       state('false', style({ transform: 'rotate(-45deg) translateY(-200%) translateX(20%)' })),
-      transition('1 <=> 0', [animate('300ms')])
+      transition('1 <=> 0', [animate('170ms')])
     ])
   ]
 })
