e2aed9a78ca66d159f3f82e3930820209e3ebf9d
Methodician <jacobgjohnston@gmail.com>
2016-12-11 11:44:25 -0800

    quick fix to menu

--- src/app/top-menu/top-menu.component.ts
+++ src/app/top-menu/top-menu.component.ts
@@ -22,7 +22,7 @@ import { Component, OnInit, trigger, state, style, transition, animate } from '@
         ]),
         trigger('topBarCollapsed', [
             state('true', style({ transform: 'rotate(0) translateX(0)', width: '*' })),
-            state('false', style({ transform: 'rotate(25deg) translateX(7px) translateY(-1px)', width: '20px' })),
+            state('false', style({ transform: 'rotate(25deg) translateX(7px) translateY(-1px)', width: '20px', height: '3px' })),
             /*
             state('true', style({ transform: 'rotate(-30deg) translateX(-3px)', width: '17px' })),
             state('false', style({ transform: 'rotate(35deg) translateY(200%) translateX(20%)' })),
@@ -33,14 +33,14 @@ import { Component, OnInit, trigger, state, style, transition, animate } from '@
         ]),
         trigger('middleBarCollapsed', [
             state('true', style({ width: '*' })),
-            state('false', style({ width: '27px' })),
+            state('false', style({ width: '27px', height: '3px' })),
             /*transition('1 <=> 0', [animate('170ms')])*/
             // for dev:
             transition('1 <=> 0', [animate('250ms')])
         ]),
         trigger('bottomBarCollapsed', [
             state('true', style({ transform: 'rotate(0) translateX(0)', width: '*' })),
-            state('false', style({ transform: 'rotate(-25deg) translateX(7px) translateY(1px)', width: '20px' })),
+            state('false', style({ transform: 'rotate(-25deg) translateX(7px) translateY(1px)', width: '20px', height: '3px' })),
             /*
             state('true', style({ transform: 'rotate(30deg) translateX(-3px)', width: '17px' })),
             state('false', style({ transform: 'rotate(-35deg) translateY(-200%) translateX(20%)' })),
