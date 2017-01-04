4c58e03c06e6262ae644f236b5a061b763969d0a
Tricia Mendoza <tvmendoza99@gmail.com>
2017-01-03 19:43:02 -0800

    removed address bar in app & tools example screenshots in apps&tools page, added _clean image versions

--- src/app/apps/apps.component.ts
+++ src/app/apps/apps.component.ts
@@ -35,87 +35,87 @@ export class AppsComponent implements OnInit {
       alt: 'community warehouse example with open menu'
     });*/
     this.cwHomeSlides.push({
-      image: '../../assets/images/ODA/cw_menu_collapsed.png',
+      image: '../../assets/images/ODA/cw_menu_collapsed_clean.png',
       text: 'menu collapsed',
       alt: 'community warehouse example with collapsed menu'
     });
     this.cwHomeSlides.push({
-      image: '../../assets/images/ODA/cw_completed_filter.png',
+      image: '../../assets/images/ODA/cw_completed_filter_clean.png',
       text: 'completed filter active',
       alt: 'community warehouse example completed filter active'
     });
     this.cwHomeSlides.push({
-      image: '../../assets/images/ODA/cw_incomplete_filter.png',
+      image: '../../assets/images/ODA/cw_incomplete_filter_clean.png',
       text: 'completed filter off',
       alt: 'community warehouse example completed filter inactive'
     });
     this.cwLockSlides.push({
-      image: '../../assets/images/ODA/cw_lock_waiver.png',
+      image: '../../assets/images/ODA/cw_lock_waiver_clean.png',
       text: 'locked area waiver',
       alt: 'community warehouse example locked area waiver'
     });
     this.cwLockSlides.push({
-      image: '../../assets/images/ODA/cw_lock_items.png',
+      image: '../../assets/images/ODA/cw_lock_items_clean.png',
       text: 'lock area items',
       alt: 'community warehouse example lock area items'
     });
     this.cwLockSlides.push({
-      image: '../../assets/images/ODA/cw_lock_summary.png',
+      image: '../../assets/images/ODA/cw_lock_summary_clean.png',
       text: 'lock area summary',
       alt: 'community warehouse example lock area summary'
     });
     this.cwLockSlides.push({
-      image: '../../assets/images/ODA/cw_lock_survey.png',
+      image: '../../assets/images/ODA/cw_lock_survey_clean.png',
       text: 'lock area survey',
       alt: 'community warehouse example lock area survey'
     });
     this.cwDetailSlides.push({
-      image: '../../assets/images/ODA/cw_req_details.png',
+      image: '../../assets/images/ODA/cw_req_details_clean.png',
       text: 'request details',
       alt: 'community warehouse example request details'
     });
     this.cwDetailSlides.push({
-      image: '../../assets/images/ODA/cw_req_items.png',
+      image: '../../assets/images/ODA/cw_req_items_clean.png',
       text: 'request items',
       alt: 'community warehouse example request items'
     });
     this.cwMaintenanceSlides.push({
-      image: '../../assets/images/ODA/cw_agencies.png',
+      image: '../../assets/images/ODA/cw_agencies_clean.png',
       text: 'agencies',
       alt: 'community warehouse example case managers'
     });
     this.cwMaintenanceSlides.push({
-      image: '../../assets/images/ODA/cw_case_managers.png',
+      image: '../../assets/images/ODA/cw_case_managers_clean.png',
       text: 'case managers',
       alt: 'community warehouse example case managers'
     });
     this.cwMaintenanceSlides.push({
-      image: '../../assets/images/ODA/cw_case_manager_edit.png',
+      image: '../../assets/images/ODA/cw_case_manager_edit_clean.png',
       text: 'case managers edit',
       alt: 'community warehouse example case managers edit'
     });
     this.gssSlides.push({
-      image: '../../assets/images/ODA/gss_filters_open.png',
+      image: '../../assets/images/ODA/gss_filters_open_clean.png',
       text: 'filters open',
       alt: 'oil camps attendent app filters open'
     })
     this.gssSlides.push({
-      image: '../../assets/images/ODA/gss_filters_closed.png',
+      image: '../../assets/images/ODA/gss_filters_closed_clean.png',
       text: 'filters closed',
       alt: 'oil camps attendent app filters closed'
     })
     this.gssSlides.push({
-      image: '../../assets/images/ODA/gss_tasks_overview.png',
+      image: '../../assets/images/ODA/gss_tasks_overview_clean.png',
       text: 'tasks overview',
       alt: 'oil camps attendent app tasks overview'
     })
     this.gssSlides.push({
-      image: '../../assets/images/ODA/gss_tasks_modal.png',
+      image: '../../assets/images/ODA/gss_tasks_modal_clean.png',
       text: 'tasks modal',
       alt: 'oil camps attendent app tasks modal'
     })
     this.gssSlides.push({
-      image: '../../assets/images/ODA/gss_notes_modal.png',
+      image: '../../assets/images/ODA/gss_notes_modal_clean.png',
       text: 'notes modal',
       alt: 'oil camps attendent app notes modal'
     })
